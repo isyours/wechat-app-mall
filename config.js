a97303598cda649528c78acfc38ea25c9f91d28f
玉鲲 <haolong.chl@alibaba-inc.com>
2019-02-20 21:20:10 +0800

    修改配置信息

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   version: "6.0.3",
   note: '接口封装',
   subDomain: "tz", // 如果你的域名是： https://api.it120.cc/abcd 那么这里只要填写 abcd
-  appid: "wxa46b09d413fbcaff", // 您的小程序的appid，购物单功能需要使用
+  appid: "wx4b5b20c91935a67b", // 您的小程序的appid，购物单功能需要使用
   shareProfile: '百款精品商品，总有一款适合您' // 首页转发的时候话术
 }
 /*
